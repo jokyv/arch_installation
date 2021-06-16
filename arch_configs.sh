7edb67f98dc1c8817c4746822b9a7c6ec0bb057b
jkyvetos <jkyvetos@oanda.com>
2021-06-16 22:02:09 +0800

    improving installation scripts

--- arch_configs.sh
+++ arch_configs.sh
@@ -16,7 +16,63 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+# or can do 
+pacman=(
+acpi 
+acpi_call 
+acpid 
+alsa-utils 
+avahi 
+base-devel 
+bash-completion 
+bluez 
+bluez-utils 
+bridge-utils 
+cups 
+dialog 
+dnsmasq 
+dnsutils 
+dosfstools 
+edk2-ovmf 
+efibootmgr 
+firewalld 
+flatpak 
+grub 
+gvfs 
+gvfs-smb 
+hplip 
+inetutils 
+ipset 
+iptables-nft 
+linux-headers 
+mtools 
+network-manager-applet 
+networkmanager 
+nfs-utils 
+nss-mdns 
+ntfs-3g 
+openbsd-netcat 
+openssh 
+os-prober 
+pipewire 
+pipewire-alsa 
+pipewire-jack 
+pipewire-pulse 
+qemu 
+qemu-arch-extra 
+reflector 
+rsync 
+sof-firmware 
+terminus-font
+tlp 
+vde2 
+virt-manager 
+wpa_supplicant 
+xdg-user-dirs 
+xdg-utils 
+    )
+
+sudo pacman -S ${pacman[@]}
 
 # add optional packages without confirming with yes
 # pacman -S --noconfirm xf86-video-amdgpu
---
+++ my_configs.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# a simple script that takes care of:
+# creating basic folders, important symlinks and configurations
+
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
+cd ~
+
+# please note:
+# bash history file moved
+# git config file moved
+# dot file should be updated as well
+
+# shouldnt i symblink the gitconfig file that i saved? 
+# configure git
+echo " "
+echo "...starting configuring git"
+git config --global user.name "John Kyvetos"
+git config --global user.email johnkyvetos@gmail.com
+# store password for 8 hours only
+git config --global credential.helper 'cache --timeout=28800'
+
+cd ~
+echo " "
+echo "..starting cloning dotfiles and creating symlinks"
+git clone https://github.com/jokyv/dotfiles.git ~/dot
+# for the HOME folder
+ln -s ~/dot/bash/.bashrc ~/.bashrc
+ln -s ~/dot/bash/.bash_profile ~/.bash_profile
+ln -s ~/dot/.xinitrc ~/.xinitrc # this has moved as well
+# for the .config/ folder
+ln -s ~/dot/tmux/ ~/.config/
+ln -s ~/dot/alacritty ~/.config/
+ln -s ~/dot/bspwm ~/.config/
+ln -s ~/dot/polybar ~/.config/
+ln -s ~/dot/sxhkd ~/.config/
+echo "Lets source all the config files"
+source ~/dot/bash/.bashrc
+
+# fonts
+cd ~
+echo " "
+echo "...Symlink my favourite fonts to ~/.local/share/"
+ln -s ~/dot/fonts/ ~/.local/share/
+
+# nvim configurations
+cd ~
+echo " "
+echo "...adding configurations and plugins for neovim "
+git clone https://github.com/jokyv/nvim.git ~/.config/
+sudo pacman -S python-pip npm xsel
+pip install pynvim
+sudo npm i -g neovim
+echo "CoC configuration and modules installation"
+echo "requies neovim and node installed"
+# Install neovim extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+  echo '{"dependencies":{}}'> package.json
+fi
+
+# Change extension names to the extensions you need
+npm install coc-json coc-python coc-snippets coc-vimlsp coc-pairs coc-jedi coc-explorer coc-sh --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+
+cd ~
+echo " "
+echo "...adding all personal repos"
+mkdir repos
+git clone https://github.com/jokyv/my_wiki.git ~/repos/
+
+# wallpapers and pics
+cd ~
+echo " "
+echo "...creating pics folder and adding my wallpapers"
+mkdir pics
+git clone https://github.com/jokyv/wallpapers ~/pics/
+echo " "
+echo "add work repos manuall!"
+echo "press [ENTER] key to continue..."
+read y
+
+
+echo " "
+echo "make sure you have conda installed first!"
+echo "pres [ENTER] key to continue"
+read y
+git clone https://github.com/jokyv/jokyv.git ~/mconda/lib/python3.8/site-packages/
+# conda and ipython configurations
+cd ~/.ipython/profile_default
+ipython profile create
+cd profile_default
+nvim ipython_config.py
+echo "uncomment autoindent and make it false"
+
+echo " "
+echo "all configurations are now done!"
