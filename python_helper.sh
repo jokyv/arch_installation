2f5776b7d59c291ec743357157c95f0e68d0e3d3
John Kyvetos <johnkyvetos@gmail.com>
2023-06-18 18:11:34 +0800

    [python_helper] add pyarrow for installation

--- python_helper.sh
+++ python_helper.sh
@@ -3,21 +3,22 @@
 set -o nounset # error when referencing undefined variable
 set -o errexit # exit when command fails
 
-# install all python packages using pip
 pip_packages=(
-  ipython
-  matplotlib
-  matplotlib-inline
+  # data analysis libraries
+  polars
   numpy
-  seaborn
   pandas
-  requests
+  pyarrow
   scikit-learn
   scipy
-  tqdm
-  mysql-connector-python
-  polars
+  ipython
 
+  # plotting libraries
+  matplotlib
+  matplotlib-inline
+  seaborn
+
+  # IDE libraries
   # use pylsp together with ruff and black
   python-lsp-server
   python-lsp-ruff
@@ -27,12 +28,34 @@ pip_packages=(
   black
   pyright
   jedi
+
+  # misc libraries
+  requests
+  tqdm
+  mysql-connector-python
+
 )
 
+install_pyenv() {
+  # reference: https://pyenv.run/    
+  set -e
+  curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+}
+
+
 # install packages only on user level
-for pip_package in ${pip_packages[@]}; do
-  echo ""
-  echo "::checking package $pip_package"
-  echo ""
-  pip install --user $pip_package
-done
+install_packages() {
+  for pip_package in ${pip_packages[@]}; do
+    echo ""
+    echo "::checking package $pip_package"
+    echo ""
+    pip install --user $pip_package
+  done
+}
+
+main() {
+  install_pyenv
+  install_packages
+}
+
+main
